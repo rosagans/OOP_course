d921564297780552841583124f4c1417c0e4f2c8
rosagans <rosagans@users.noreply.github.com>
2026-09-10 03:07:48 +0000

    Deploying to gh-pages from @ rosagans/OOP_course@ffa855508341af6687adc833e4c4a5c482fcd885 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
